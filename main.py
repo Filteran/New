999b988a8536e5cd48b170aeea2b5ec74b6300dc
Filteran <106659796+Filteran@users.noreply.github.com>
2022-12-04 16:58:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ async def add_footer(bot: Client, event: Message):
         await AddFooter(bot, event, footer_text, int(on_event))
 
 
-@AHBot.on_message(filters.channel & filters.text & ~filters.edited & ~filters.private, group=-1)
+@AHBot.on_message(filters.channel & filters.text & ~filters.private, group=-1)
 async def add_text_footer(bot: Client, event: Message):
     on_event = await db.find_user_id(event.chat.id)
     if on_event is None:
